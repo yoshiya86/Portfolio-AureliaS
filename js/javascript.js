9db1f4f42f5ce32ce9c731e5d5ab0905bf310d38
Yoshiya86 <aureliasandii@gmail.com>
2024-10-30 16:38:32 +0700

    update 30/10

---
+++ js/javascript.js
@@ -0,0 +1,28 @@
+let slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs((slideIndex += n));
+}
+
+function showDivs(n) {
+  let i;
+  let x = document.getElementsByClassName("prev");
+  if (n > x.length) {
+    slideIndex = 1;
+  } else if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex - 1].style.display = "block";
+  return;
+}
+
+function on() {
+  document.getElementById("gd1").style.display = "block";
+}
+function off() {
+  document.getElementById("gd1").style.display = "none";
+}
